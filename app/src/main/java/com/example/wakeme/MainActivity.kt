8de75c3724a9f3e4845cac806b4edb9178cdb612
kobegeens <kobegeens@proton.me>
2025-05-11 23:56:02 +0200

    made and referenced to an xml stylesheet

--- app/src/main/java/com/example/wakeme/MainActivity.kt
+++ app/src/main/java/com/example/wakeme/MainActivity.kt
@@ -17,31 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            WakeMeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.mainactivity)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WakeMeTheme {
-        Greeting("Android")
     }
 }
