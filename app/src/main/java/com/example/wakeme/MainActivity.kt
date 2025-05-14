1d5f76588384738335c2aa54a3896a617eefe74c
kobegeens <kobegeens@proton.me>
2025-05-14 22:19:30 +0200

    worked on layout and fixed error

--- app/src/main/java/com/example/wakeme/MainActivity.kt
+++ app/src/main/java/com/example/wakeme/MainActivity.kt
@@ -11,13 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.wakeme.ui.theme.WakeMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.mainactivity)
-        }
     }
 }
